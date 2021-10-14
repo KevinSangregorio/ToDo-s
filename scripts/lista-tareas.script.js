512356668f8e09c074b69374bed6cf44123b49ac
KevinSangregorio <kesangregorio@gmail.com>
2021-10-14 12:27:24 -0300

    [ADD] Agrego proyecto completo junto al informe explicativo

---
+++ scripts/lista-tareas.script.js
@@ -0,0 +1,95 @@
+window.onload = () => {
+  // agregarTarea();
+  getTareas();
+
+  document.forms.agregarTarea.addEventListener('submit', event => {
+    event.preventDefault();
+    agregarTarea()
+  });
+}
+
+comprobarToken();
+setInterval(() => {
+  comprobarToken();
+}, 100000)
+
+function comprobarToken() {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    location.href = './login.html'
+  }
+}
+
+function getTareas() {
+  RequestManager.get('/tasks').then(tareas => {
+    crearTareas(tareas);
+  })
+}
+
+function crearTareas(tareas) {
+  document.querySelector('ul.tareas-pendientes').innerHTML = '';
+  document.querySelector('ul.tareas-terminadas').innerHTML = '';
+
+  tareas.forEach(tarea => {
+    renderizarTarea(tarea)
+  })
+}
+
+function renderizarTarea(tarea) {
+  const template = `
+    <li class="tarea animar-entrada">
+      <div class="not-done" onclick='completarTarea(${tarea.id}, ${tarea.completed})'></div>
+      <div class="descripcion">
+        <p class="nombre">${tarea.description}</p>
+        <p class="timestamp">Creado el: ${tarea.createdAt}</p>
+        <button class="eliminar"  onclick='eliminarTarea(${tarea.id})'>Eliminar</button>
+      </div>
+    </li>
+  `;
+
+  const contenedorTareasPendientes = document.querySelector('ul.tareas-pendientes');
+  const contenedorTareasCompletas = document.querySelector('ul.tareas-terminadas');
+  if (!tarea.completed) {
+    contenedorTareasPendientes.innerHTML += template;
+  } else {
+    contenedorTareasCompletas.innerHTML += template;
+  }
+}
+
+function agregarTarea() {
+  const descripcion = document.forms.agregarTarea.descripcionNuevaTarea.value;
+  const body = {
+    description: descripcion,
+    completed: false
+  }
+
+  RequestManager.post(`/tasks`, body)
+    .then(tarea => {
+      renderizarTarea(tarea);
+    }).catch(err => {
+      console.log(err);
+    })
+}
+
+function completarTarea(id, completed) {
+  const body = { completed: !completed };
+  RequestManager.put(`/tasks/${id}`, body)
+    .then(tarea => {
+      getTareas();
+    }).catch(err => {
+      console.log(err)
+    })
+}
+
+function eliminarTarea(id) {
+  if (!confirm('Esta seguro que desea eliminar la tarea?')) {
+    return;
+  }
+
+  RequestManager.delete(`/tasks/${id}`).then(tarea => {
+    getTareas();
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
